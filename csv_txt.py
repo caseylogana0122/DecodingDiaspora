844a4a8c2f175ae0755b852633bdcc423e02035f
Emersyn Harris <eharr21@u.rochester.edu>
2023-03-09 15:49:50 -0500

    spelling fix

--- csv_txt.py
+++ csv_txt.py
@@ -4,7 +4,7 @@ import os
 with open(r'CSV_Progress.csv') as f:
     with open('metadata_of_corpus.txt', 'w') as md:
         #column names
-        md.write("dcterms:title,dcterms:date,dcterms:spacial")
+        md.write("dcterms:title,dcterms:date,dcterms:spatial")
 
         csvreader = csv.reader(f, delimiter=',')
         next(csvreader)
