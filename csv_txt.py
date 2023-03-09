748730bb773af96ad9ac8944182aa94710f5b3e8
caseylogana0122 <108601919+caseylogana0122@users.noreply.github.com>
2023-03-09 14:20:20 -0600

    script in process

---
+++ csv_txt.py
@@ -0,0 +1,20 @@
+import csv
+import os
+
+with open(r'CSV_Progress.csv') as f:
+    with open('metadata_of_corpus.txt', 'w') as md:
+        #column names
+        md.write("dcterms:title,dcterms:date,dcterms:spacial")
+        #skips first line in CSV
+        next(f)
+
+        for line in f:
+            newrow = "FN-" + line[0] + line[1] + "," + line[2] + "," + line[3]
+            md.write(newrow)
+
+print("complete")
+            
+
+
+
+  
